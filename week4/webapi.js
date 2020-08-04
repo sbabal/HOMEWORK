e90cab399d688a41b96f6690fed77322093717a0
sunny babal <sbabal@hotmail.com>
2020-08-04 09:15:25 -0500

    My week5 web API Homework

---
+++ week4/webapi.js
@@ -0,0 +1,59 @@
+
+// Need to create a button
+// Click a Button Event 
+// On Click Timer starts
+// Set timer for 3 minute activity
+// Record the score in a variable
+// Record intitials somewhere
+// Click will ASK questions.
+// Questions can be part of a function or Array??
+// If correct answer -----then move to the next questions
+// Else is incorrect then take time out of timer (Example if 60 seconds remaining minus 10 seconds to make it 50 seconds)
+// If all questions answered OR TIMER REACHES 0, then End activity 
+// Log Score and initials
+
+
+
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+//show welcome screen
+//user hits button - EVETN
+//start quiz - FUNCTION
+//loop 5 itmes ---5 quetions (loop # of question in array) 5 times
+//show questions FUNCTKION
+//get user answer evetn "user clicking butto"
+//check user answer
+//update score / update timer
+
+
+//Global Variabls
+ var question [];
+
+ var score = 0;
+var question
+
+//functions
+
+Function showWelcomeScreen ()
+fnctko
+
+function startQuiz() {
+   for (let i = 0; i < questions.length; i++) {
+       showQuestion()
+    
+   }
+
+}
+
+//events
+
+
+
+//CODE ENTRUY 
+show welcome screen()
+
+
+
+
